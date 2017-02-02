57a7930d2875be812bbd228eb3745c50a18d40e5
christopher.hawk <christopher.hawk@ucalgary.ca>
2017-02-02 16:12:52 -0700

    Turned off debug flag.

--- src/main/java/aim4/ShoutAheadAI/predicates/ObstacleLT10MetersNorth.java
+++ src/main/java/aim4/ShoutAheadAI/predicates/ObstacleLT10MetersNorth.java
@@ -1,5 +1,10 @@
 package aim4.ShoutAheadAI.predicates;
-
+/**
+ * This predicate is true if the driver agent perceives an obstacle less than 10 meters North of it's current position. 
+ * 
+ * @author christopher.hawk
+ *
+ */
 public class ObstacleLT10MetersNorth implements Predicate {
 
 	@Override
